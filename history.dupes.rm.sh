4f0a101b87d672528851a261b8a450fba21175a8
Vsevolod Tsiliurik <tsilvs@proton.me>
2024-06-08 16:22:53 +0100

    new history dupes cleaner

--- history.dupes.rm.sh
+++ history.dupes.rm.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# Create a temporary file
-temp_file=$(mktemp)
-
-# Read the history file line by line
-while IFS= read -r line
-do
-  # If the line starts with a '#', it's a timestamp, so just write it to the temp file
-  if [[ "$line" =~ ^# ]]; then
-    echo "$line" >> "$temp_file"
-  else
-    # If the line is a command, only write it to the temp file if it's not a duplicate
-    if ! grep -qx "$line" "$temp_file"; then
-      echo "$line" >> "$temp_file"
-    fi
-  fi
-done < ~/.bash_history
-
-# Replace the original history file with the cleaned one
-mv "$temp_file" ~/.bash_history
-
-# Reload the history into the current shell
+cp ~/.bash_history ~/.bash_history.bak &&
+temp_file=$(mktemp) &&
+history | awk -F'\\$>' '!seen[$2]++' | sed -E 's/^[ ]*[0-9]+[ ]+[0-9-]+[ ]+[0-9:]+[$]+[>]+ //' > "$temp_file" &&
+history -c &&
+mv "$temp_file" ~/.bash_history &&
 history -r ~/.bash_history
+
+# TODO
+# [ ] Read `awk -F` from current `$HISTTIMEFORMAT`
